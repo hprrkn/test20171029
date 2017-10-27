8838e23a55cf53e18509f9ae0e50bd59c4877187
prrknh <prrknh@gmail.com>
2017-10-28 01:43:16 +0900

    initialize repository

---
+++ src/main/java/com/tubers/tubers/TubersApplication.java
@@ -0,0 +1,12 @@
+package com.tubers.tubers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TubersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TubersApplication.class, args);
+	}
+}
